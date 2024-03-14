025019c21230040e952eaa0b5d7ad90c72833a4e
Lopun <lopun.jh@gmail.com>
2024-03-15 00:54:31 +0900

    necessary libraries added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from "tailwindcss";
+import withMT from "@material-tailwind/react/utils/withMT";
 
 const config: Config = {
   content: [
+    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -15,6 +17,7 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
-export default config;
+
+export default withMT(config);
